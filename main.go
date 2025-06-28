bfa5fad89f3b8fcdb6dd61fac4ec787e638adae7
Stas Lagoda <lagoda.stasik@gmail.com>
2025-06-28 11:46:44 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type Data struct {
+	Weight int `json:"weight"`
+	Length int `json:"length"`
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := Data{
+			Weight: rand.Intn(30) + 1,  // weight от 1 до 30
+			Length: rand.Intn(100) + 1, // length от 1 до 100
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(data)
+	})
+
+	http.ListenAndServe(":33500", nil)
+}
